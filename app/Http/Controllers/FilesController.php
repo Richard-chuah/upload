944b5174c13a74772345c673898f979a056c86b3
Richard <chuahrc@strateqgroup.com>
2022-01-24 17:09:53 +0800

    initial commit

---
+++ app/Http/Controllers/FilesController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\File;
+use Illuminate\Http\Request;
+use Illuminate\Http\UploadedFile;
+use App\Http\Requests\StoreFileRequest;
+use Illuminate\Support\Facades\Storage;
+
+class FilesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //$files = File::all();
+        $files = [];
+        return view('files.index', [
+            'files' => $files
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('files.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  StoreFileRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreFileRequest $request)
+    {
+        $time = time();
+        $fileName = $time . '.'. $request->file->extension();  
+
+        $type = $request->file->getClientMimeType();
+        $size = $request->file->getSize();
+        $file = $request->file;
+        //$request->file->move(public_path('file'), $fileName);
+        Storage::disk('ftp')->put($time, $file);
+        
+        /* File::create([
+            'user_id' => 1,
+            'name' => $fileName,
+            'type' => $type,
+            'size' => $size
+        ]); */
+
+        return redirect()->route('files.index')->withSuccess(__('File added successfully.'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\File  $file
+     * @return \Illuminate\Http\Response
+     */
+    public function show(File $file)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\File  $file
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(File $file)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\File  $file
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, File $file)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\File  $file
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(File $file)
+    {
+        //
+    }
+}
